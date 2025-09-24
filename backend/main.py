eeee811b09b059701399b2ad9ee72bba0d8b2ed0
sigmoidblue <whykrishwhy@gmail.com>
2025-09-24 17:52:23 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # can temporarily use ["*"] during testing
+    allow_origins=["*"],  # can temporarily use ["*"] during testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
